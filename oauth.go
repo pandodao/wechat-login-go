ec15ea49f5da030c163e689aa387c2b3ab140cb3
xuhui <xuhuiandfish@gmail.com>
2023-04-18 12:11:25 +0800

    add user info query

--- oauth.go
+++ oauth.go
@@ -16,11 +16,33 @@ type (
 		IsSnapshotUser int    `json:"is_snapshotuser"`
 		UnionID        string `json:"unionid"`
 	}
+
+	UserInfoResp struct {
+		OpenID     string   `json:"openid"`
+		NickName   string   `json:"nickname"`
+		Sex        int      `json:"sex"`
+		Province   string   `json:"province"`
+		City       string   `json:"city"`
+		Country    string   `json:"country"`
+		Headimgurl string   `json:"headimgurl"`
+		Privilege  []string `json:"privilege"`
+		UnionID    string   `json:"unionid"`
+	}
 )
 
 func (c *Client) OAuthLogin(ctx context.Context, code string) (*OAuthLoginResp, error) {
 	output := OAuthLoginResp{}
 	uri := fmt.Sprintf("%s?appid=%s&secret=%s&code=%s&grant_type=authorization_code", "/sns/oauth2/access_token", c.appID, c.appSecret, code)
+	if err := c.request(ctx, http.MethodGet, uri, nil, &output); err != nil {
+		return nil, err
+	}
+
+	return &output, nil
+}
+
+func (c *Client) QueryUserinfo(ctx context.Context, accessToken, openID, lang string) (*UserInfoResp, error) {
+	output := UserInfoResp{}
+	uri := fmt.Sprintf("%s?access_token=%s&openid=%s&lang=%s", "/sns/userinfo", accessToken, openID, lang)
 	if err := c.request(ctx, http.MethodGet, uri, nil, &output); err != nil {
 		return nil, err
 	}
